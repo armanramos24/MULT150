06dfbdb0853b553c122bde88144b974703dc6f1e
armanramos24 <a.ramos1717@gmail.com>
2026-04-26 21:30:50 -0700

    Created Hour 10

---
+++ ARamos_Hour10/Assets/GameCritic.cs
@@ -0,0 +1,21 @@
+/*
+
+1. After trying it out for myself, I think the game in terms of seems fine. It's way too simple to really call it difficult, but there is a bit of fumbling
+trying to get that last ball into the goal. All in all, I will say the game is a little too easy, solely just based on the simplicity and you cannot lose at all.
+
+2. The first thing I think that would increase the difficulty is a timer. The added stress while also trying to fumble around trying to corral the last couple balls
+would really add to its difficulty.
+
+3. For me, I think more hazards or obstacles scattered around the playing area would add not only to the randomness of bounce angles for all balls, but also adds a layer
+of approach strategy for the player to move around and adjust to.
+
+4. I think overall the game is fun, but the most tedious parts to me are the movespeed of the bumper, and the bouciness of the game balls. It feels jarringly slow for how 
+the rest of the game moves in my honest opinion. It feels like it rides the fence of wanting to be an all-out fast-paced game but the registered physics restrict it a bit.
+
+Change Suggestions:
+- Changed the size of the bumper axis X from 3.5 to 2.5, and axis Y from 3 to 4
+- Added more game objects as obstacles for balls to bounce around more
+- Changed size of two chaos balls to the same size as the normal balls
+- Changed footsteps sound file to match walking in water
+
+*/
