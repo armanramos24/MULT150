7da3be1987799e83d0a103d2fe6d1e4dc73bba59
armanramos24 <a.ramos1717@gmail.com>
2026-05-17 00:07:11 -0700

    Hour 14 and 16

---
+++ ARamos_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,10 @@
+/* Parameters Modified:
+ 
+- Changed color of particles to a gradient of purple to blue
+- Changed emission rate over time to 25
+- Changed shape to hemisphere
+- Added size over lifetime, changed curve editor to decrease size over time
+- Added size by speed, changed curve editor to increase spead with size
+- Added noise, changed frequency to 1, changed scroll speed to 2
+
+*/
---
+++ ARamos_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,12 @@
+/* Modifications Made;
+
+- Imported new background image
+- Changed color of Title font
+- Changed font type of Title
+- Changed font type of Subtitle
+- Changed Title Text to "Faewild Fighters"
+- Changed Subtitle Text to "Dimensional Drift"
+- Changed Start button color to light green
+
+
+*/
