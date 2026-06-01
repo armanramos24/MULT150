c0a019267d430c1395bb4d02d77cb40988963079
armanramos24 <a.ramos1717@gmail.com>
2026-05-31 19:24:21 -0700

    Final Project

---
+++ ARamos_Final/Assets/Modifications.cs
@@ -0,0 +1,74 @@
+﻿public class Modifications
+{/*
+    ░▒▓████████▓▒░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓██████▓▒░ ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░ */
+
+
+    // Choose 1 of the games below and list modifications 
+    // Delete the other 2 games
+
+
+    /*  ================================================================
+        CHAOS BALL
+        ================================================================
+        1 - directional light (colour, intensity, etc)
+        modifications: 
+    - changed color to a darker haze
+    - tilted angle
+    - changed intensity to .3
+
+        2 - fog
+        modifications: set to linear, changed color value
+
+        3 - create & apply skybox and customise it (atmosphere thickness, sky tint, exposure, etc)
+        modifications: 
+    - applied skybox and edited tint and exposure
+
+        CHAOS BALLS
+        4 - convert a chaos ball into a prefab, and replace all of them in the scene with instances of that prefab
+        [no record necessary]
+
+        5 - add particle system to the chaos ball prefab and customise at least 3 parameters
+        -All instances will update to include the particle system
+        coloured balls
+        parameter 1: emission
+        parameter 2: shape
+        parameter 3: size over lifetime
+        parameter 4: rotation over lifetime
+
+        6 - convert a coloured ball into a prefab, create variants for each of the colours, and replace them in the scene
+        -Don't forget to update the tags so that the game works
+        [no record necessary]
+
+        7 - apply at least 1 new texture while retaining the different colours
+        (add source url or package name)
+        texture source: https://www.magnific.com/free-photo/distressed-yellow-wall-texture-background_4390946.htm#fromView=keyword&page=1&position=0&uuid=95a843df-5b75-4b64-9cdb-fb6de5657d41&query=Yellow+texture
+
+        GOALS
+        8 - modify light components (colour, range, intensity, etc)
+        modifications: 
+        - changed color to more saturated, giving a white center. 
+        - changed range to 15
+        - changed intensity to 5
+
+        WALL
+        9 - convert each to complex objects comprised of 3 or more shapes
+        -Try recreating them to be more interesting! castle walls, fences, pillars, etc
+        [no record necessary]
+
+        10 - use gameobject hierarchy to keep the wall prefab organised & apply at least 1 new texture
+        (add source url or package name)
+        texture source: https://www.textures.com/download/floor-streets-0098/38431
+
+    */
+
+
+
+
+
+}
